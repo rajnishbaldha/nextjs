619486edf113bdaf0b86eca67550a26eb584e551
Rajnish Baldha <sciontechnospace@gmail.com>
2022-10-12 17:25:36 +0530

    Reset page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,8 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome!
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
       </main>
 
       <footer className={styles.footer}>
